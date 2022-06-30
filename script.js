79f41554d792fcd0dd7f15cd895f5f5b89635b66
itsrasfa <rafaelacontatoss@gmail.com>
2022-06-30 02:18:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// Destructuring no Javascript
+
+const carro = {
+  marca: "Hyundai",
+  modelo: "Creta",
+  cor: "Preto",
+  ano: "2022",
+  informacoes: {
+    placa: "FYR7745",
+    garantia: true,
+    km: "0km"
+  }
+}
+
+/*
+const marca = carro.marca;
+const modelo = carro.modelo;
+const cor = carro.cor;
+const informacoes = carro.informacoes.placa
+*/
+
+const { marca, modelo, informacoes: { placa }, informacoes: { km }, ...outras_informacoes } = carro;
+console.log(marca, placa, outras_informacoes)
+
+// criando destructuring na assinatura da função
+const possuiGarantia = ({ informacoes: { garantia } }) => garantia
+
+console.log(possuiGarantia(carro))
+
+
+// Destructuring em arrays
+const top_linguagens = ["Python", "Java", "PHP", "JavaScript", "C++"]
+
+const [, , , linguagem_favorita] = top_linguagens;
+console.log(linguagem_favorita)
